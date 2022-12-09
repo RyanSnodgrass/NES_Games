2cd98f27bf7e5eea08482011a9846aa7416683d4
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2022-12-09 10:43:18 -0500

    Chapter 12 Practical Loops

--- src/helloworld.asm
+++ src/helloworld.asm
@@ -24,24 +24,21 @@
   STX PPUADDR
   LDX #$00
   STX PPUADDR
-  LDA #$29
+load_palettes:
+  LDA palettes,X
   STA PPUDATA
-  LDA #$19
-  STA PPUDATA
-  LDA #$09
-  STA PPUDATA
-  LDA #$0f
-  STA PPUDATA
+  INX
+  CPX #$04
+  BNE load_palettes
 
   ; write sprite data
-  LDA #$70
-  STA $0200 ; Y-coord of first sprite
-  LDA #$05
-  STA $0201 ; tile number of first sprite
-  LDA #$00
-  STA $0202 ; attributes of first sprite
-  LDA #$80
-  STA $0203 ; X-coord of first sprite
+  LDX #$00
+load_sprites:
+  LDA sprites,X
+  STA $0200,X
+  INX
+  CPX #$04
+  BNE load_sprites
 
 vblankwait:       ; wait for another vblank before continuing
   BIT PPUSTATUS
@@ -57,6 +54,13 @@ forever:
 
 .segment "VECTORS"
 .addr nmi_handler, reset_handler, irq_handler
+
+.segment "RODATA"
+palettes:
+.byte $29, $19, $09, $0f
+
+sprites:
+.byte $70, $05, $00, $80
 
 .segment "CHR"
 .incbin "graphics.chr"
